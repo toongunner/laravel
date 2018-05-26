4fbe674ab22a55c7b6a134fd5444660d83dff4f9
toongunner <37064532+toongunner@users.noreply.github.com>
2018-05-26 18:19:52 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '209.97.160.241'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'fetwpzwyrd'),
             'username' => env('DB_USERNAME', 'fetwpzwyrd'),
